e559d9ac0dbbe71f2e37b4d143f39a27f0024123
Mikael Valot <mikael.valot+github@gmail.com>
2016-12-04 22:01:59 +0000

    improved ros-teleop, DRY with the ros-indigo-zt yml

--- ros-indigo-zt/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo service zerotier-one start
-: "${ZT_NETWORK:?Need to set ZT_NETWORK}"
-sudo zerotier-cli join $ZT_NETWORK
-source ros_entrypoint.sh
-bash -c "$1"
--- ros-teleop/run.sh
+++ ros-teleop/run.sh
@@ -1,1 +1,3 @@
-docker-compose run teleop $1
+docker-compose up -d master echo 
+docker logs echo -f &
+docker-compose run --rm --name teleop teleop
