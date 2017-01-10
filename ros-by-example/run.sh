f11b2c6d6cc8533479514534e6ca739cf70ec5b1
Mikael Valot <mikael.valot@gmail.com>
2017-01-10 21:13:32 +0000

    ir ok

--- ros-by-example/run.sh
+++ ros-by-example/run.sh
@@ -1,4 +1,6 @@
 docker build -t ros-by-example .
-docker-compose up -d  master ev3_launch
-#docker-compose run --rm --name ev3_launch ev3_launch
+docker-compose up -d  master 
+echo Please start ev3_manager on ev3
+read -p "Press any key to continue... " -n1 -s
+docker-compose up ev3_launch
 
